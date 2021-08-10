25c5f714f1f384883bceef2dd8f284a8b7cb76ae
alfred <alfred.tupaz@myxlog.com>
2021-08-10 21:49:33 +0800

    UI fixes

--- pages/index.js
+++ pages/index.js
@@ -258,7 +258,7 @@ const Home = () => {
       <div className="w-full custom-bg-color text-white contact">
         <div className="container mx-auto py-20 lg:flex flex-row justify-around items-start">
           <div className="lg:w-6/12 w-full lg:text-left text-center mx-auto">
-            <div className="lg:pl-24 px-4 mx-auto">
+            <div className="lg:pl-16 px-4 mx-auto">
               <h1 className="text-5xl font-bold">We’re always</h1>
               <h1 className="text-5xl font-bold mt-4"> happy to help!</h1>
               <p className="mt-8 lg:mr-auto xl:w-8/12 lg:w-5/6">Your concerns are important to us. Complete the form and we will get back to you as soon as possible.</p>
