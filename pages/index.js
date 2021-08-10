3845d609186b231961ff6523376dc0e536662482
alfred <alfred.tupaz@myxlog.com>
2021-08-10 21:42:50 +0800

    UI fixes

--- pages/index.js
+++ pages/index.js
@@ -165,7 +165,7 @@ const Home = () => {
         <div className="container mx-auto  text-center px-4">
           <h1 className="lg:text-6xl text-5xl font-bold">Merchants</h1>
           <p className="text-base mt-4 mx-auto xl:w-1/3 lg:w-2/5">Shop from your favourite merchants across the country and enjoy 0% interest <span className="xl:inline-block lg:inline-block  block">for 5 monthly installment.</span></p>
-          <div className="lg:px-10 lg:flex md:grid md:grid-rows-2 md:grid-flow-col md:gap-8 lg:gap-0  flex-row justify-evenly lg:space-x-8 mt-12 lg:text-auto text-center">
+          <div className="lg:px-10 lg:flex md:grid md:grid-rows-2 md:grid-flow-col md:gap-8 lg:gap-0 flex-row justify-evenly lg:space-x-8 mt-12 lg:text-auto text-center">
             <div className="shadow-lg flex items-center justify-center rounded lg:mx-0 mx-auto lg:mb-0 mb-4 merchant-logo-size transform transition duration-500 hover:scale-105 hover:shadow-sm" style={{backgroundColor:'#EADDBB'}}>
               <div className="relative w-4/5 h-4/5 flex items-center justify-center">
                 <Image
@@ -200,7 +200,7 @@ const Home = () => {
             </div>
           </div>
 
-          <div className="lg:px-10 lg:flex flex-row justify-center lg:space-x-8 mt-12 md:grid md:grid-rows-2 md:grid-flow-col md:gap-8 lg:gap-0">
+          <div className="lg:px-10 lg:flex md:grid md:grid-rows-2 md:grid-flow-col md:gap-8 lg:gap-0 flex-row justify-evenly lg:space-x-8 mt-12 lg:text-auto text-center">
             <div className="shadow-lg flex bg-white  items-center justify-center rounded lg:mx-0 mx-auto lg:mb-0 mb-4 merchant-logo-size transform transition duration-500 hover:scale-105 hover:shadow-sm">
               <div className="relative w-4/5 h-4/5 flex items-center justify-center">
                 <Image
